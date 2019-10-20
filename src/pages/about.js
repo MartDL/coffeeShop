0880dabc82b8a56d92405477b1a581290ff97c2d
Martin Lord <Mart1981@Marts-MacBook-Air.local>
2019-10-20 13:50:38 +0100

    Connected to Contenful

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,7 +4,7 @@ import { Link, graphql } from "gatsby"
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 import BackgroundSection from '../components/Globals/BackgroundSection';
-import Info from '../components/Home/Info';
+import Info from '../components/Home/info';
 
 const AboutPage = ( { data }) => (
   <Layout>
